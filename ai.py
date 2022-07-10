d6cc88f3ecf99c2245ae89b70a007d9c1d75de7b
thirawat27 <thunthirawas@gmail.com>
2022-07-10 18:12:15 +0700

    first commit

--- ai.py
+++ ai.py
@@ -1,5 +1,5 @@
 from gtts import gTTS
 import playsound
-tts = gTTS(text='สวัสดีค่ะ,ยินดีที่ได้รู้จักค่ะ,ฉันคือaiของคอมเครื่องนี้ค่ะ', lang='th')
+tts = gTTS(text='สวัสดีค่ะ,ยินดีที่ได้รู้จักค่ะ,ฉันชื่อสมศรีค่ะ', lang='th')
 tts.save("sound.mp3")
 playsound.playsound("sound.mp3", True)
